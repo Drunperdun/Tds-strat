4236b311fbaac91a2961a0ec32b40252bc6aabaf
Drunperdun <eptakykyepta@gmail.com>
2026-07-08 20:16:36 +0300

    AetherHub.lua

--- AetherHub.lua
+++ AetherHub.lua
@@ -2085,24 +2085,63 @@ local Configuration = Window:Tab({Title = "Configuration", Icon = "sliders-horiz
     Configuration:Section({Title = "Experimental Features"})
     
     Configuration:Toggle({
-        Title = "Sticker Spam",
-        Desc = "This will drop everyones FPS to like 5 (you will not be able to see this unless you have an alt)",
+        Title = "Sticker Spam (Max)",
+        Desc = "Максимально агрессивная версия. Сильно роняет FPS у всех вокруг. Используй с осторожностью!",
         Value = false,
         Callback = function(v)
-            StickerSpam = v
+            _G.StickerSpam = v
 
-            if StickerSpam then
-                task.spawn(function()
-                    while StickerSpam do
-                        for i = 1, 9999 do
-                            if not StickerSpam then break end
+            if _G.StickerSpam then
+                local ReplicatedStorage = game:GetService("ReplicatedStorage")
+                local StickerRemote = ReplicatedStorage:WaitForChild("Network"):WaitForChild("Sticker"):WaitForChild("URE:Show")
 
-                            local args = {"Flex"}
-                            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Sticker"):WaitForChild("URE:Show"):FireServer(unpack(args))
+                local Stickers = {
+                    "Flex", "Chilly", "Shrug", "CHOMP", "Not The Same",
+                    "Worried Scout", "Cookie Scout", "Lock In", "GG",
+                    "Silly", "Dead Stare", "Quack", "WOW"
+                }
+
+                -- Запускаем 12 агрессивных потоков
+                for i = 1, 12 do
+                    task.spawn(function()
+                        while _G.StickerSpam do
+                            for _, name in ipairs(Stickers) do
+                                if not _G.StickerSpam then break end
+                                pcall(function()
+                                    StickerRemote:FireServer(name)
+                                end)
+                            end
+                            task.wait()
                         end
-                        task.wait()
+                    end)
+                end
+
+                -- Дополнительный супер-быстрый поток на Flex
+                task.spawn(function()
+                    while _G.StickerSpam do
+                        for i = 1, 600 do
+                            if not _G.StickerSpam then break end
+                            pcall(function()
+                                StickerRemote:FireServer("Flex")
+                            end)
+                        end
+                        task.wait(0.02)
                     end
                 end)
+
+                Window:Notify({
+                    Title = "Lag Machine",
+                    Desc = "Максимальный спам запущен (13 потоков)",
+                    Time = 4,
+                    Type = "normal"
+                })
+            else
+                Window:Notify({
+                    Title = "Lag Machine",
+                    Desc = "Остановлено",
+                    Time = 3,
+                    Type = "normal"
+                })
             end
         end
     })
@@ -4539,4 +4578,4 @@ end
 
 MissionsUIFix()
 
-return TD
+return TDS
